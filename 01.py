e76dbf5b5af49afa559e46087c283bcc3e822401
Hans <hans.schuelein@stud-mail.uni-wuerzburg.de>
2021-12-01 10:25:10 +0100

    fixed the issue in 01 and added more alternatives

--- 01.py
+++ 01.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+import itertools
+
 import aoc
+import more_itertools
 
 
 class Window:
+    """A sliding window that can be filled, checked for fullness and its sum taken."""
+
     cache: list[int | None]
     head = 0
 
@@ -14,24 +19,23 @@ class Window:
         self.cache[self.head] = value
         self.head = self.head + 1 if self.head + 1 != len(self.cache) else 0
 
-    def has_any_elements(self) -> None:
+    def is_full(self) -> None:
         try:
-            self.get_average()
+            self.get_sum()
             return True
         except AttributeError:
             return False
 
-    def get_average(self) -> float:
-        non_nones = [element for element in self.cache if element is not None]
-        if not non_nones:
+    def get_sum(self) -> float:
+        if None in self.cache:
             raise AttributeError("No element in window yet")
-        return float(sum(non_nones)) / len(non_nones)
+        return sum(self.cache)
 
 
 def main() -> None:
     numbers = aoc.get_integers()
 
-    # part 1
+    # Part 1
     current_number = None
     increment_counter = 0
     for number in numbers:
@@ -39,20 +43,40 @@ def main() -> None:
             if number > current_number:
                 increment_counter += 1
         current_number = number
-    print(increment_counter)
+    print("Part one, number of increments>", increment_counter)
 
-    # part 2
+    # Part 2 --- custom sliding window class based
     window = Window(3)
     increment_counter = 0
     for number in numbers:
         current_number = None
-        if window.has_any_elements():
-            current_number = window.get_average()
+        if window.is_full():
+            current_number = window.get_sum()
         window.add(number)
-        if current_number is not None:
-            if window.get_average() > current_number:
+        if window.is_full() and current_number is not None:
+            if window.get_sum() > current_number:
                 increment_counter += 1
-    print(increment_counter)
+    print("Part 2 number of increments (class based)>", increment_counter)
+
+    # Part 2 --- more_itertools based
+    current_number = None
+    increment_counter = 0
+    for window in more_itertools.triplewise(numbers):
+        new_sum = sum(window)
+        if current_number is not None:
+            if new_sum > current_number:
+                increment_counter += 1
+        current_number = new_sum
+    print("Part 2 number of increments (more_itertools based)>", increment_counter)
+
+    # Part 2 --- more more_itertools based
+    increment_counter = sum(
+        [
+            sum(after) > sum(before)
+            for before, after in itertools.pairwise(more_itertools.triplewise(numbers))
+        ]
+    )
+    print("Part 2 number of increments (more more_itertools based)>", increment_counter)
 
 
 if __name__ == "__main__":
