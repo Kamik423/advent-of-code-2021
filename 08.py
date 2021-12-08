7e75649e1bbe25c341129729aab812004dc2caf3
Hans <hans.schuelein@stud-mail.uni-wuerzburg.de>
2021-12-08 14:01:07 +0100

    day 8

---
+++ 08.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+
+import aoc
+
+
+"""
+ aaaa
+b    c
+b    c
+ dddd
+e    f
+e    f
+ gggg
+"""
+
+
+def code_length(number: int) -> int:
+    """How long does the code for a number have to be."""
+    return [6, 2, 5, 5, 4, 5, 6, 3, 7, 6][number]
+
+
+def required_letters(number: int) -> str:
+    """The segments required for a certain number."""
+    return [
+        "abcefg",
+        "cf",
+        "acdeg",
+        "acdfg",
+        "bcdf",
+        "abdfg",
+        "abdefg",
+        "acf",
+        "abcdefg",
+        "abcdfg",
+    ][number]
+
+
+def opposite_code(code: str) -> str:
+    """The opposite code to a certain code."""
+    return "".join([c for c in "abcdefg" if c not in code])
+
+
+def main() -> None:
+    lines = aoc.get_lines()
+    # lines = [
+    #     "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+    # ]
+
+    # part 1
+    print(
+        sum(
+            sum(len(word) in [2, 3, 4, 7] for word in line.split(" | ")[1].split(" "))
+            for line in lines
+        )
+    )
+
+    # part 2
+    big_sum = 0
+    for line in lines:
+        words = list(
+            set("".join(sorted(word)) for word in line.split(" ") if word != "|")
+        )
+
+        # real segment name : what could still be it
+        candidates = {key: list("abcdefg") for key in "abcdefg"}
+
+        # number : digits that might be in it
+        symbols = ["abcdefg" for _ in range(10)]
+
+        def might_be(number: int, candidate: str) -> bool:
+            """Might this number still be this string candidate.
+
+            This checks if all of the segments in the candidate still lie on the
+            number and the ones not included can all lie off the number.
+
+            This however might match all candidate segments to the same real
+            one. This is not a problem however as no number will be eliminated
+            just on this.
+            """
+            if not code_length(number) == len(candidate):
+                return False
+            missing = opposite_code(candidate)
+            for required_letter in required_letters(number):
+                if not any(c in candidate for c in candidates[required_letter]):
+                    return False
+            for no_letter in opposite_code(required_letters(number)):
+                if not any(
+                    c in opposite_code(candidate) for c in candidates[no_letter]
+                ):
+                    return False
+            return True
+
+        def is_the_only_number(code: str, number: int, others: list[str] = []) -> bool:
+            """Is this the only number among the candidates that matches the code."""
+            others = others or [n for n in range(10) if n != number]
+            return might_be(number, code) and not any(might_be(n, code) for n in others)
+
+        def register(number: int, code: str) -> None:
+            """Register a code to a number and update relations knowledge."""
+            symbols[number] = code
+            for key in opposite_code(required_letters(number)):
+                candidates[key] = [c for c in candidates[key] if c not in code]
+            # print(f"{number} <== {code}")
+
+        def unfound(number: int) -> bool:
+            """Has this number not yet been found."""
+            if number == 8:
+                return False
+            return len(symbols[number]) == 7
+
+        def certain_candidate(code: str) -> int | None:
+            """Is this the only certain candidate. Otherwise None."""
+            code = "".join(sorted(code))
+            matching_symbols = [n for n, symbol in enumerate(symbols) if symbol == code]
+            if len(matching_symbols) == 1:
+                return matching_symbols[0]
+            return None
+
+        # repeatedly see if this number is the only one that could match a word.
+        # 10x because there are at most 10 rounds of finding numbers
+        for _ in range(10):
+            for number in range(10):
+                # only try to match as of yet undetermined numbers
+                if unfound(number):
+                    for word in words:
+                        # If this is the only number that could still be be this
+                        # code then register it. Remove the word from the list
+                        # of candidates
+                        if is_the_only_number(word, number):
+                            register(number, word)
+                            words.remove(word)
+
+        # Reconstruct output number
+        accumulator = 0
+        for output_code in line.split(" | ")[1].split(" "):
+            accumulator *= 10
+            new = certain_candidate(output_code)
+            if new is None:
+                print("WARNING")
+            else:
+                accumulator += new
+
+        big_sum += accumulator
+
+    print(big_sum)
+
+
+if __name__ == "__main__":
+    main()
