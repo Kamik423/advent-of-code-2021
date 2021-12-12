45a42dd0490e7a91919c07d4976f7f2760ca3ca9
Hans <hans.schuelein@stud-mail.uni-wuerzburg.de>
2021-12-12 21:46:44 +0100

    day 11

---
+++ 11.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import itertools
+
+import aoc
+
+
+def main() -> None:
+    lines = aoc.get_lines()
+
+    matrix = [[int(character) for character in line] for line in lines]
+    width = len(matrix[0])
+    height = len(matrix)
+
+    flashes = 0
+    for step in itertools.count(1):
+        for y in range(height):
+            for x in range(width):
+                matrix[y][x] += 1
+        requires_update = True
+        can_change = [[True for _ in range(width)] for _ in range(height)]
+        while requires_update:
+            requires_update = False
+            for y in range(height):
+                for x in range(width):
+                    if can_change[y][x] and matrix[y][x] > 9:
+                        requires_update = True
+                        can_change[y][x] = False
+                        flashes += 1
+                        for y2 in [y - 1, y, y + 1]:
+                            for x2 in [x - 1, x, x + 1]:
+                                if 0 <= x2 < width and 0 <= y2 < height:
+                                    if can_change[y2][x2]:
+                                        matrix[y2][x2] += 1
+                        matrix[y][x] = 0
+        if step == 100:
+            print(flashes)
+        if not any(any(row) for row in can_change):
+            print(step)
+            break
+
+
+if __name__ == "__main__":
+    main()
