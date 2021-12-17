ee5aea18db0b817085a3ca4621027aae8ffaf1d1
Hans <hans.schuelein@stud-mail.uni-wuerzburg.de>
2021-12-17 13:06:16 +0100

    split out 17 plot in own file

--- 17-plot.py
+++ 17-plot.py
@@ -10,17 +10,13 @@ from joblib import Parallel, delayed
 from PIL import Image
 
 
-def main(timer: aoc.Timer) -> None:
+def main() -> None:
     xmin, xmax, ymin, ymax = aoc.Parse().regex_lines(
         r"target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)..(-?\d+)", (int, int, int, int)
     )[0][0]
     # xmin, xmax, ymin, ymax = 20, 30, -10, -5
 
     start_velocity = -ymin - 1
-    hightest_y = int(start_velocity * (start_velocity + 1) / 2)
-    print(hightest_y)
-
-    timer.mark()
 
     # lower and upper bounds
     vx_lb = int(ceil(0.5 * (sqrt(8 * xmin) - 1)))
@@ -59,15 +55,25 @@ def main(timer: aoc.Timer) -> None:
         return State(vx, vy).hits()
 
     print(
-        sum(
-            Parallel(n_jobs=2)(
-                delayed(test)(vx, vy)
-                for vx, vy in product(range(vx_lb, vx_ub + 1), range(vy_lb, vy_ub + 1))
-            )
-        )
+        f"{1 + vx_ub - vx_lb} x {1 + vy_ub - vy_lb}"
+        f" = {(1 + vx_ub - vx_lb)*(1 + vy_ub - vy_lb)} states"
     )
+    print("Range>", vx_lb, vx_ub, vy_lb, vy_ub)
+
+    # Image plot
+    Image.fromarray(
+        np.array(
+            [
+                [
+                    np.array([255, 255, 255], dtype=np.uint8) * test(vx, vy)
+                    + np.array([255, 0, 0], dtype=np.uint8) * (vx == 0 or vy == 0)
+                    for vx in range(vx_ub + 1)
+                ]
+                for vy in reversed(range(vy_lb, vy_ub + 1))
+            ]
+        )
+    ).save("17-results.png")
 
 
 if __name__ == "__main__":
-    with aoc.Timer() as timer:
-        main(timer)
+    main()
